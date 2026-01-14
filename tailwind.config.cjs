f69b38df3514fa46a127df5a1e7a48fb1f4bc64e
ryandyla <57823856+ryandyla@users.noreply.github.com>
2026-01-14 22:35:36 +0000

    update postcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
