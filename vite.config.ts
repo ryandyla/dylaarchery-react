21c91ab8ef5d312a9e434e961cb748bc45f636cf
ryandyla <57823856+ryandyla@users.noreply.github.com>
2026-01-14 09:49:50 +0000

    more clean

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "src/react-app",
+  root: "/src",
   plugins: [react()],
   build: {
     outDir: "./dist",
