d35647163d2f13262af36d08afc0f58d21ba8c6e
ryandyla <57823856+ryandyla@users.noreply.github.com>
2026-01-19 22:17:13 +0000

    try to fix tailwind again

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [cloudflare(), react(), tailwindcss()],
 });
+
 
 
